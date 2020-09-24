96bc1d54c584a6bd327e2d6acdd6de3c9f43dc36
Philipp Kutin <philipp.kutin@gmail.com>
2020-09-24 09:04:51 +0200

    Add observation about using LuaJIT from Ubuntu 20.04 repo.

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,9 @@ LUA_PATH="$d/?/init.lua;$d/?.lua"
 
 export LUA_PATH
 
+# With LuaJIT from Ubuntu 20.04 repo,
+#  LuaJIT 2.1.0-beta3 -- Copyright (C) 2005-2017 Mike Pall.
+# bug does not occur.
+
 # weird bug: happens with -O3, but not with -O2
 luajit -O3 "$d/tests.lua" "$@"
