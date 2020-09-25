924b5f1feebdb5e661692f5867a6d76162dd2a6e
Philipp Kutin <philipp.kutin@gmail.com>
2020-09-25 16:20:49 +0200

    Move TEST_LOOP_COUNT into run_tests.sh (keeping user-facing 'make' commands)
    'make test-loop' is now the same as 'TEST_LOOP_COUNT=2000 make test'.

--- Makefile
+++ Makefile
@@ -4,12 +4,10 @@
 do_test := $(SHELL) ./run_tests.sh
 
 test: libselect_dummy.so
-	$(do_test)
+	@$(do_test)
 
-TEST_LOOP_COUNT ?= 2000
-test-loop: test
-	@echo "INFO: Repeating for a total of $(TEST_LOOP_COUNT) runs."
-	@i=1; while test $$i -lt $(TEST_LOOP_COUNT); do i=$$((i+1)); $(do_test); done
+test-loop:
+	@TEST_LOOP_COUNT=2000 $(do_test)
 
 opt_warn_flags := -O2
 opt_warn_flags += -Werror -Wall -Wextra -pedantic
--- run_tests.sh
+++ run_tests.sh
@@ -10,5 +10,14 @@ export LD_LIBRARY_PATH="$d"
 #  LuaJIT 2.1.0-beta3 -- Copyright (C) 2005-2017 Mike Pall.
 # bug does not occur.
 
+loop_count=${TEST_LOOP_COUNT:=1}
+if [ $loop_count -gt 1 ]; then
+    echo "INFO: Repeating for a total of $loop_count runs."
+fi
+
 # weird bug: happens with -O3, but not with -O2
-luajit -O3 "$d/tests.lua" "$@"
+i=0;
+while [ $i -lt $loop_count ]; do
+    i=$((i+1));
+    luajit -O3 "$d/tests.lua" "$@"
+done
